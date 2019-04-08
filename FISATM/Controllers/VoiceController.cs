3185e4ec8c50e2829fa13694085842a36a70f1c2
Bhupendra Kumar Shukla <ssbks55@gmail.com>
2019-04-09 01:10:32 +0530

    Add files via upload

---
+++ FISATM/Controllers/VoiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FISATM.Controllers
+{
+    public class VoiceController : Controller
+    {
+        // GET: Voice
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
